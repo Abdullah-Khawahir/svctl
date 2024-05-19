b0d083c89729ddec18579bb4e71c9520da7a29cb
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2024-05-19 11:08:13 +0300

    added test for yaml file parsing

--- uploader/uploader_test.go
+++ uploader/uploader_test.go
@@ -10,39 +10,93 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var test *testing.T
 
+func TestArtifactConfigValidation(t *testing.T) {
+	// WARNING: this test will fail if the formatter formattes the strings in
+	//	    the createFile function so be aware
+	passingFilepath := createFile(t, "good.yaml", `
+artifacts:
+  - name: handler1
+    path: /path/to/source1
+    destination: http://path/to/destination1
+  - name: handler2
+    path: /path/to/source2
+    destination: http://path/to/destination1
+`)
+	c, err := InitializeArtifactConfig(passingFilepath) // err = nil
+	assert.NoError(t, err, "the file contains all requirment it must not fail if it does update the requirment and update this message")
+	h1, h2 := c.Handlers[0], c.Handlers[1]
+	assert.Equal(t, h1.Name, "handler1")
+	assert.Equal(t, h2.Name, "handler2")
+
+	assert.Equal(t, h1.SourceRegex, "/path/to/source1")
+	assert.Equal(t, h2.SourceRegex, "/path/to/source2")
+
+	assert.Equal(t, h1.Destination, "http://path/to/destination1")
+	assert.Equal(t, h2.Destination, "http://path/to/destination1")
+
+	failingFilePath := createFile(t, "bad.yaml", `
+artifacts:
+	- path: /path/to/source1
+	destination: /path/to/destination1
+	- name: handler2
+	destination: http://path/to/destination1
+	- name: handler3
+	path: /path/to/source2
+		`)
+
+	_, err1 := InitializeArtifactConfig(failingFilePath) // err = somthing
+	assert.EqualError(t, err1, "each handler must have a name")
+
+	defer os.Remove(failingFilePath)
+	defer os.Remove(passingFilepath)
+}
 func TestFailedFiles(t *testing.T) {
-	SetFileAsFailedToUpload("f1.txt")
-	SetFileAsFailedToUpload("f2.txt")
+	handler := ArtifactHandler{
+		Name:         "test",
+		ArtifactList: []string{"f1.txt", "f2.txt"},
+	}
+	SetFileAsFailedToUpload(handler, "f1.txt")
+	SetFileAsFailedToUpload(handler, "f2.txt")
 	expected := []string{"f1.txt", "f2.txt"}
-	got := GetFailedFiles("./f1.txt")
+	got := GetFailedFiles(handler)
 	if !slices.Equal(expected, got) {
 		t.Errorf("expected %s but got %s", expected, got)
 	}
-	defer deleteRecordFiles()
+	defer deleteRecordFiles(handler)
 }
 func TestSentFiles(t *testing.T) {
 	os.Remove(SuccessfulUploadFile)
-	SetFileAsFailedToUpload("f1.txt")
-	SetFileAsFailedToUpload("f2.txt")
+	handler := ArtifactHandler{
+		Name:         "test",
+		ArtifactList: []string{"f1.txt", "f2.txt"},
+	}
+	SetFileAsFailedToUpload(handler, "f1.txt")
+	SetFileAsFailedToUpload(handler, "f2.txt")
 	expected := []string{"f1.txt", "f2.txt"}
-	got := GetFailedFiles("./f1.txt")
+	got := GetFailedFiles(handler)
 	if !slices.Equal(expected, got) {
 		t.Errorf("expected %s but got %s", expected, got)
 	}
-	defer deleteRecordFiles()
+	defer deleteRecordFiles(handler)
 }
 func TestSentFilesEmpty(t *testing.T) {
 	os.Remove(SuccessfulUploadFile)
+	handler := ArtifactHandler{
+		Name:         "test",
+		ArtifactList: []string{"f1.txt", "f2.txt"},
+	}
 	var expected []string = nil
-	got := GetFailedFiles("./f1.txt")
+	got := GetFailedFiles(handler)
 	if !slices.Equal(expected, got) {
 		t.Errorf("expected %s but got %s", expected, got)
 	}
-	defer deleteRecordFiles()
+	defer deleteRecordFiles(handler)
 }
 
 func TestHttpSecuredUpload(t *testing.T) {
@@ -77,7 +131,7 @@ func TestHttpSecuredUpload(t *testing.T) {
 	}
 
 	config.uploadFiles()
-	defer deleteRecordFiles()
+	defer deleteRecordFiles(config.Handlers[0])
 }
 func TestHttpUpload(t *testing.T) {
 	test = t
@@ -125,7 +179,8 @@ func TestHttpUpload(t *testing.T) {
 	}
 
 	config.uploadFiles()
-	defer deleteRecordFiles()
+	defer deleteRecordFiles(config.Handlers[0])
+	defer deleteRecordFiles(config.Handlers[1])
 }
 
 func createFile(t *testing.T, filename string, fileContent string) string {
@@ -156,7 +211,7 @@ func handleLogFiles(w http.ResponseWriter, r *http.Request) {
 		test.Errorf("expected log file but got %s", string(body))
 	}
 }
-func deleteRecordFiles() {
-	os.Remove(FailedUploadsFile)
-	os.Remove(SuccessfulUploadFile)
+func deleteRecordFiles(h ArtifactHandler) {
+	os.Remove(h.getTrackingFailedFile())
+	os.Remove(h.getTrackingSuccessfulFile())
 }
